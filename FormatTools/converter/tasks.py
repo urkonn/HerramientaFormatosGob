41dc97afde0831beca776d772f23e532bfe1ddd6
Frank <akura11.tt@gmail.com>
2016-07-19 18:45:23 -0500

    Correciones redis docker

--- FormatTools/converter/tasks.py
+++ FormatTools/converter/tasks.py
@@ -10,10 +10,10 @@ def transform_file(self, file_path, format_to, xls_name):
     Task Celery que se encarga del proceso
     de transformacion de los xls y xlsx
     """
-    try:
-        converter_engine = XLSConverter.get_converter(format_to.encode('utf-8'), file_path.encode('utf-8'), xls_name.encode('utf-8'))
-    except Exception, file_error:
-        raise Exception(file_error)
+    #try:
+    converter_engine = XLSConverter.get_converter(format_to.encode('utf-8'), file_path.encode('utf-8'), xls_name.encode('utf-8'))
+    #except Exception, file_error:
+    #    raise Exception(file_error)
 
     # Inicio de proceso de conversion
     for progress in converter_engine.convert():
