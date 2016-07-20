da4fa64f3751d5077b9ab5f050cae24884cb5799
Frank <akura11.tt@gmail.com>
2016-07-19 19:15:52 -0500

    Correciones redis docker

--- FormatTools/converter/views.py
+++ FormatTools/converter/views.py
@@ -47,7 +47,7 @@ def download_file(request, path, file_name):
     # Se recupera el archivo temporal
     try:
         path_base = os.path.join(settings.TEMPORAL_FILES_ROOT, path)
-        file = open(os.path.join(path_base, file_name.encode('utf-8')))
+        file = open(os.path.join(path_base, unicode(file_name, 'utf-8')))
     except IOError, e:
         raise Http404
 
